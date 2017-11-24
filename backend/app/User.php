625c2a5cb7eae42ef74eff61d0f7ac7831eada68
Alex Stansfield <alex.stansfield@gmail.com>
2017-11-25 02:18:49 +0700

    Auth Api (#29)

--- backend/app/User.php
+++ backend/app/User.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
+
+    protected $table = 'user';
 
     /**
      * The attributes that are mass assignable.
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/auth', 'AuthController@auth');
+Route::get('/auth/test', 'AuthController@test')->middleware('auth:api');
