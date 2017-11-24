7055c61ca59c7ef3902b496d834bf84071a38fd9
Jeremy Lamit <jeremy.lamit@zenrooms.com>
2017-11-24 22:47:32 +0700

    SemanticUI
    https://semantic-ui.com
    
    SemanticUI-React
    
    https://react.semantic-ui.com

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import logo from '../../logo.svg';
+import {Container, Menu, Segment} from 'semantic-ui-react'
 
 export default class Header extends Component {
-  render () {
-    return (
-      <header>
-        This is header
-      </header>
-    )
-  }
+    state = {activeItem: 'home'};
+    handleItemClick = (e, {name}) => this.setState({activeItem: name});
+
+    render() {
+        const {activeItem} = this.state;
+        return (
+            <Menu pointing secondary fixed='top' size='large'>
+                <Container>
+                    <Menu.Item name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick}>
+                        <img src={logo} className="OpenDesign-Logo" alt="OpenDesign"/>
+                    </Menu.Item>
+                    <Menu.Menu position='right'>
+                        <Menu.Item name='Guidelines' active={activeItem === 'Guidelines'}
+                                   onClick={this.handleItemClick}/>
+                        <Menu.Item name='sign-up' active={activeItem === 'sign-up'}
+                                   onClick={this.handleItemClick}/>
+                        <Menu.Item name='login' active={activeItem === 'login'}
+                                   onClick={this.handleItemClick}/>
+                    </Menu.Menu>
+                </Container>
+            </Menu>
+        )
+    }
 }
