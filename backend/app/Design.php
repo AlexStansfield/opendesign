db95733cded1b6e85a5abc2ee372e1adf27c9c91
Alex Stansfield <alex.stansfield@gmail.com>
2017-11-25 04:08:40 +0700

    Design Like Api (#31)

--- backend/app/Design.php
+++ backend/app/Design.php
@@ -4,10 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Design
+ * @package App
+ */
 class Design extends Model
 {
     /**
      * @var string Table name
      */
-    protected $table='design';
+    protected $table = 'design';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function likes()
+    {
+        return $this->hasMany('App\Like');
+    }
 }
