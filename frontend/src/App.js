6c67fe2cc58462310bd9551292b01afcc5adc505
Jeremy <jeremy.lamit@zenrooms.com>
2017-11-25 02:23:35 +0700

    Home page (#28)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { Container } from 'semantic-ui-react'
+import {Grid} from 'semantic-ui-react'
 import Header from './components/Header'
 import Main from './components/Main'
 
-
 class App extends Component {
-  render() {
-    return (
-      <Provider store={this.props.store}>
-        <Container style={{ marginTop: '4em' }}>
-          <Header />
-          <Main />
-        </Container>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={this.props.store}>
+	            <Grid columns={1}>
+  	              <Grid.Column>
+    	                <Grid.Row style={{marginBottom: '5em'}}>
+      	                  <Header/>
+        	            </Grid.Row>
+          	          <Grid.Row>
+            	            <Main/>
+              	      </Grid.Row>
+                	</Grid.Column>
+	            </Grid>
+						</Provider>
+        );
+    }
 }
 
 export default App;
--- frontend/src/containers/Home/index.js
+++ frontend/src/containers/Home/index.js
@@ -1,14 +1,94 @@
-import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import {Container, Header, Grid, Button, Item, Image} from 'semantic-ui-react'
+import './home.css'
 
 export default class Home extends Component {
-  render () {
-    console.log('Homepage props', this.props)
+    render() {
+        console.log('Homepage props', this.props)
 
-    return (
-      <Container>
-        This is homepage, bros!
-      </Container>
-    )
-  }
+        return (
+            <Container>
+                <Header>Welcome to Open Design!</Header>
+                <p>Choose a project you would like to contribute some design, helps to, or ... <Button primary>submit a
+                    new project</Button></p>
+                <Item.Group>
+                    <Container>
+                        <Grid columns={3} celled className='projectCard'>
+                            <Grid.Row>
+                                <Grid.Column width={3} className='image'>
+                                    <Image floated='left' src='https://dummyimage.com/211x164/000000/fff.png'/>
+                                </Grid.Column>
+                                <Grid.Column width={10}>
+                                    <Header as='h2'>
+                                        Project #1
+                                    </Header>
+                                    <Header as='h3'>
+                                        Lorem ipsum dolor sit amet
+                                    </Header>
+                                    Sed bibendum dapibus justo vel egestas. Aliquam erat volutpat. Vivamus eget suscipit
+                                    erat, sit amet suscipit est. Fusce a velit facilisis, finibus diam ornare, vehicula
+                                    nibh.
+                                </Grid.Column>
+	                            <Grid.Column width={3}>
+		                            <p><Button fluid basic color='blue' content='Add brief' icon='add' /></p>
+		                            <p><Button fluid basic color='blue' content='Comment' icon='comment' label={{ as: 'a', basic: true, color: 'blue', pointing: 'left', content: '100' }} /></p>
+		                            <p><Button fluid basic color='blue' content='View details' icon='add' /></p>
+	                            </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Container>
+	                <Container>
+		                <Grid columns={3} celled className='projectCard'>
+			                <Grid.Row>
+				                <Grid.Column width={3} className='image'>
+					                <Image floated='left' src='https://dummyimage.com/211x164/000000/fff.png'/>
+				                </Grid.Column>
+				                <Grid.Column width={10}>
+					                <Header as='h2'>
+						                Project #2
+					                </Header>
+					                <Header as='h3'>
+						                Lorem ipsum dolor sit amet
+					                </Header>
+					                Sed bibendum dapibus justo vel egestas. Aliquam erat volutpat. Vivamus eget suscipit
+					                erat, sit amet suscipit est. Fusce a velit facilisis, finibus diam ornare, vehicula
+					                nibh.
+				                </Grid.Column>
+				                <Grid.Column width={3}>
+					                <p><Button fluid basic color='blue' content='Add brief' icon='add' /></p>
+					                <p><Button fluid basic color='blue' content='Comment' icon='comment' label={{ as: 'a', basic: true, color: 'blue', pointing: 'left', content: '100' }} /></p>
+					                <p><Button fluid basic color='blue' content='View details' icon='add' /></p>
+				                </Grid.Column>
+			                </Grid.Row>
+		                </Grid>
+	                </Container>
+	                <Container>
+		                <Grid columns={3} celled className='projectCard'>
+			                <Grid.Row>
+				                <Grid.Column width={3} className='image'>
+					                <Image floated='left' src='https://dummyimage.com/211x164/000000/fff.png'/>
+				                </Grid.Column>
+				                <Grid.Column width={10}>
+					                <Header as='h2'>
+						                Project #3
+					                </Header>
+					                <Header as='h3'>
+						                Lorem ipsum dolor sit amet
+					                </Header>
+					                Sed bibendum dapibus justo vel egestas. Aliquam erat volutpat. Vivamus eget suscipit
+					                erat, sit amet suscipit est. Fusce a velit facilisis, finibus diam ornare, vehicula
+					                nibh.
+				                </Grid.Column>
+				                <Grid.Column width={3}>
+					                <p><Button fluid basic color='blue' content='Add brief' icon='add' /></p>
+					                <p><Button fluid basic color='blue' content='Comment' icon='comment' label={{ as: 'a', basic: true, color: 'blue', pointing: 'left', content: '100' }} /></p>
+					                <p><Button fluid basic color='blue' content='View details' icon='add' /></p>
+				                </Grid.Column>
+			                </Grid.Row>
+		                </Grid>
+	                </Container>
+                </Item.Group>
+            </Container>
+        )
+    }
 }
